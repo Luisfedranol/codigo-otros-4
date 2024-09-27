d3c25ee1db73434303e7f69cb03e888afa8ebb32
Luis Pedraza <luisfedranol@gmail.com>
2024-09-27 17:44:04 -0600

    Se termina el ejercicio

--- src/com/generation/Codigo4.java
+++ src/com/generation/Codigo4.java
@@ -17,27 +17,36 @@ public class Codigo4 {
 	       System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
 	       String j2 = s.nextLine().toLowerCase(); // Convertir a minúsculas para uniformidad
 	       
-	       if (j1 == j2) {
+	       if (j1.equals(j2)) {
 	    	      System.out.println("Empate");
 	    	    } else {
 	    	    	 int g = 2;
 		    	      switch (j1) {
 					case "piedra": {
-						
+						if (j2 == "tijera") {
+				            g = 1;
+				          }
+						 System.out.println("Gana el jugador " + g);
 						break;
 					}
 					case "papel": {
-						
+						if (j2 == "piedra") {
+				            g = 1;
+				          }
+						 System.out.println("Gana el jugador " + g);
 						break;
 					}
 					case "tijera": {
-						
+						if (j2 == "papel") {
+				            g = 1;
+				          }
+						 System.out.println("Gana el jugador " + g);
 						break;
 					}
 					default:
-						throw new IllegalArgumentException("Unexpected value: " + j1);
-					}
-		    	       
+						 System.out.println("Entrada inválida");
+					}//switch       
 				}//else     
+	       s.close(); //Se cierra el Scanner
 	}//main
 }//class Codigo4
